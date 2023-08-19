74d103deb2d600cc837afd7b89e923097a307fae
bartosz <bartosz@bartosz1999.local>
2023-08-19 14:00:50 +0200

    Changed views names

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { authGuard } from "@auth0/auth0-vue";
-import HomeView from "../views/HomeView.vue";
+const Auth = () => import("../views/AuthView.vue");
 const Home = () => import("../views/HomeView.vue");
-const Aboute = () => import("../views/AboutView.vue");
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: "/auth",
+      name: "auth",
+      component: Auth,
+    },
+    {
       path: "/",
       name: "home",
       component: Home,
-    },
-    {
-      path: "/about",
-      name: "about",
-      component: Aboute,
       beforeEnter: authGuard,
     },
   ],
---
+++ src/views/AuthView.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { useAuth0 } from "@auth0/auth0-vue"
+const { loginWithRedirect } = useAuth0();
+const handleLogin = () => {
+  loginWithRedirect()
+}
+const handleSignup = () => {
+  loginWithRedirect({
+    authorizationParams: {
+      screen_hint: "signup"
+    }
+  })
+}
+</script>
+
+<template>
+  <div class="flex h-screen">
+    <div class="relative flex flex-1 flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
+      <div class="absolute top-12 flex items-center gap-1">
+        <h1 class="font-semibold text-xl">Chat<span class="text-blue-600">BMW</span></h1>
+        <img class="h-6" src="../assets/4140436_bmw_logo_icon.png" alt="">
+      </div>
+      <div class="mx-auto w-full max-w-sm lg:w-96">
+        <div class="flex justify-center">
+          <h1 class="mt-6 text-4xl font-bold tracking-tight text-gray-900">Get Started</h1>
+        </div>
+        <div class="mt-8">
+          <div class="mt-6 flex gap-4">
+            <button @click="handleLogin" type="button"
+              class="inline-flex items-center rounded-md border border-transparent bg-blue-600 px-16 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">Log
+              in</button>
+            <button @click="handleSignup" type="button"
+              class="inline-flex items-center rounded-md border border-transparent bg-blue-600 px-16 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">Sign
+              up</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="relative hidden w-0 flex-1 lg:block">
+      <img class="absolute inset-0 h-full w-full object-cover" src="../assets/pexels-jay-pizzle-3849554.jpg" alt="" />
+    </div>
+  </div>
+</template>
