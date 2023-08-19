9927233e65460c2a7e11b1bf37b44ff004c1323a
bartosz <bartosz@bartosz1999.local>
2023-08-19 11:35:09 +0200

    Home page ui

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-  </header>
-
   <RouterView />
 </template>
