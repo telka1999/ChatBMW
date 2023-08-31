0af9df16ec7d2a3a6aee7a1dab47c123adcaf8a0
bartosz <bartosz@bartosz1999.local>
2023-08-31 08:07:30 +0200

    Modyfied pacagejson to deploye to vercel

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ sequelize.sync();
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "/frontend/dist/index.html")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
